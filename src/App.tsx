96809b1ad0c8629006e423e33ac9b38b44ae341b
JinJu <jin_jooo@naver.com>
2025-02-23 21:05:33 +0900

    Add setting files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css';
+
+function App() {
+  return (
+    <>
+      <div className={'bg-amber-400 w-screen h-screen'}>Hello World</div>
+    </>
+  );
+}
+
+export default App;
